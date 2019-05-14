7ee92ba570fbe923141cf6a1b40f2c655173df44
briandchinn <bchinn310@gmail.com>
2019-05-14 15:13:23 -0700

    adds long and lat to table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_14_170304) do
+ActiveRecord::Schema.define(version: 2019_05_14_191907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2019_05_14_170304) do
     t.datetime "updated_at", null: false
     t.string "middle_name"
     t.text "bio"
+    t.float "longitude"
+    t.float "latitude"
   end
 
 end
